34380bc563da142550a9b9c57c36c5b6511de907
Eric Greene <eric@t4d.io>
2020-12-09 14:17:42 -0500

    Add code demos

---
+++ demo-app/src/components/CarTool.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function CarTool() {
+  return (
+    <header>
+      <h1>Car Tool</h1>
+    </header>
+  );
+}
--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { HelloWorld } from './components/HelloWorld';
+import { CarTool } from './components/CarTool';
 
 ReactDOM.render(
-  React.createElement(HelloWorld),
+  // React.createElement(React.Fragment, null,
+  //   React.createElement(HelloWorld),
+  //   React.createElement(CarTool));
+  <>
+    <HelloWorld />
+    <CarTool />
+  </>,
   document.querySelector('#root'),
 );
 
