24cb64c78f966ea09cd0ffc16cdd65af750dca23
Eric Greene <eric@t4d.io>
2020-12-09 13:57:37 -0500

    add exercise 1

---
+++ demo-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { HelloWorld } from './components/HelloWorld';
+
+ReactDOM.render(
+  React.createElement(HelloWorld),
+  document.querySelector('#root'),
+);
+
+
