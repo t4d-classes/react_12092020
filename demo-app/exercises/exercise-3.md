b0e71048195c1454192f4051d5435f5cc9cc504c
Eric Greene <eric@t4d.io>
2020-12-09 15:15:18 -0500

    Add exercise 3

---
+++ demo-app/exercises/exercise-3.md
@@ -0,0 +1,7 @@
+# Exercise 3
+
+## Steps
+
+1. Construct an array of car objects INSIDE of car tool component using the data from the car table. Dynamically build the table rows with the array of car objects.
+
+2. Ensure it works!
--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 export function ColorTool() {
 
+  const colors = [
+    { id: 1, name: 'red', hexcode: 'ff0000' },
+    { id: 2, name: 'green', hexcode: '00ff00' },
+    { id: 3, name: 'blue', hexcode: '0000ff' },
+  ];
+
+  const colorListItems = colors.map(color =>
+    <li key={color.id}>{color.name}</li>)
+
   return (
     <>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        <li>blue</li>
-        <li>green</li>
-        <li>red</li>
-        <li>purple</li>
-        <li>yellow</li>
-        <li>orange</li>
-        <li>gray</li>
-        <li>black</li>
+        {colorListItems}
       </ul>
     </>
   )
