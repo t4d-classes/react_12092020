f4e27bb81d6dc42cc4f875d59dbd2cff7fd776b0
Eric Greene <eric@t4d.io>
2020-12-09 14:37:37 -0500

    add exercise 2

---
+++ demo-app/src/components/ColorTool.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export function ColorTool() {
+
+  return (
+    <>
+      <header>
+        <h1>Color Tool</h1>
+      </header>
+      <ul>
+        <li>blue</li>
+        <li>green</li>
+        <li>red</li>
+        <li>purple</li>
+        <li>yellow</li>
+        <li>orange</li>
+        <li>gray</li>
+        <li>black</li>
+      </ul>
+    </>
+  )
+
+}
--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { HelloWorld } from './components/HelloWorld';
+import { ColorTool } from './components/ColorTool';
 import { CarTool } from './components/CarTool';
 
 ReactDOM.render(
   // React.createElement(React.Fragment, null,
-  //   React.createElement(HelloWorld),
+  //   React.createElement(ColorTool),
   //   React.createElement(CarTool));
   <>
-    <HelloWorld />
+    <ColorTool />
     <CarTool />
   </>,
   document.querySelector('#root'),
