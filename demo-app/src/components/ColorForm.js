c9a9bfd7d265a0b1c70eed717a421e20226fad15
Eric Greene <eric@t4d.io>
2020-12-10 12:23:31 -0500

    add exercise 5

--- demo-app/src/components/ColorForm.js
+++ demo-app/src/components/ColorForm.js
@@ -6,12 +6,13 @@ export function ColorForm() {
   const [
     colorForm, // state data
     setColorForm, // a function which updates the state data and triggers a re-render
-  ] = useState({ name: '' } /* initial state of the form on the first render */);
+  ] = useState({ name: '', hexcode: '', } /* initial state of the form on the first render */);
 
   const change = (e) => {
 
     setColorForm({
-      name: e.target.value,
+      ...colorForm, // copy over the properties from the color form
+      [e.target.name]: e.target.value, // update the one property for the field I am typing into
     });
 
   };
@@ -22,7 +23,11 @@ export function ColorForm() {
     <form>
       <div>
         <label htmlFor="color-name-input">Color Name</label>
-        <input type="text" id="color-name-input" value={colorForm.name} onChange={change} />
+        <input type="text" id="color-name-input" name="name" value={colorForm.name} onChange={change} />
+      </div>
+      <div>
+        <label htmlFor="color-name-input">Color Hexcode</label>
+        <input type="text" id="color-name-input" name="hexcode" value={colorForm.hexcode} onChange={change} />
       </div>
     </form>
   );
