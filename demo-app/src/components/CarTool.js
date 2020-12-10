f39152cfe7af801aa70d89274c2f712d13961d89
Eric Greene <eric@t4d.io>
2020-12-10 12:44:04 -0500

    Add code demos

--- demo-app/src/components/CarTool.js
+++ demo-app/src/components/CarTool.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 import { ToolHeader } from './ToolHeader';
 import { CarTable } from './CarTable';
+import { CarForm } from './CarForm';
 
 export function CarTool(props) {
   return (
     <>
       <ToolHeader headerText="Car Tool" />
       <CarTable cars={props.cars} />
+      <CarForm />
     </>
   );
 }
